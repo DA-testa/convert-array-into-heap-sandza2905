7eb20efbc73de50c79fb8fca77f510c9c66ad390
221RDB004 Sandis Bērziņš <125198711+SandisBerzins221RDB004@users.noreply.github.com>
2023-03-20 15:58:05 +0200

    Update build_heap.py

--- build_heap.py
+++ build_heap.py
@@ -1,48 +1,51 @@
-def heap_sort(arr):
-    swaps = []
-    n = len(arr)
+def heap_sort(data):
+    def heapify(data, n, i):
+        lielakais = i
+        kreisais = 2 * i + 1
+        labais = 2 * i + 2
 
-    def heapify(start, end):
-        smallest = start
-        left = 2 * start + 1
-        right = 2 * start + 2
+        if kreisais < n and data[kreisais] > data[lielakais]:
+            lielakais = kreisais
 
-        if left < end and arr[left] < arr[smallest]:
-            smallest = left
+        if labais < n and data[labais] > data[lielakais]:
+            lielakais = labais
 
-        if right < end and arr[right] < arr[smallest]:
-            smallest = right
+        if lielakais != i:
+            data[i], data[lielakais] = data[lielakais], data[i]
+            heapify(data, n, lielakais)
 
-        if smallest != start:
-            swaps.append((start, smallest))
-            arr[start], arr[smallest] = arr[smallest], arr[start]
-            heapify(smallest, end)
+    n = len(data)
 
+    
     for i in range((n // 2) - 1, -1, -1):
-        heapify(i, n)
+        heapify(data, n, i)
 
-    return swaps
+    
+    for i in range(n - 1, 0, -1):
+        data[0], data[i] = data[i], data[0]
+        heapify(data, i, 0)
 
+    return data
 
 def main():
-    input_type = input("Enter 'I' for input or 'F' for file: ")
+    input_type = input("I or F: ")
 
     if input_type == "I":
-        n = int(input("Enter the number of elements: "))
-        arr = list(map(int, input("Enter space-separated integers: ").split()))
+        n = int(input())
+        data = list(map(int, input().split()))
+
     elif input_type == "F":
-        filename = input("Enter the filename: ")
+        filename = input("File name: ")
         with open(f"tests/{filename}", "r") as f:
             n = int(f.readline())
-            arr = list(map(int, f.readline().split()))
+            data = list(map(int, f.readline().split()))
 
-    assert len(arr) == n
-    swaps = heap_sort(arr)
+    assert len(data) == n
 
-    print("Number of swaps:", len(swaps))
-    for i, j in swaps:
-        print(i, j)
+    sorted_data = heap_sort(data)
 
+    for element in sorted_data:
+        print(element)
 
 if __name__ == "__main__":
     main()
